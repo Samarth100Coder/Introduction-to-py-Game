65372073042389f343a74db52fb6e504bf548a68
Samarth100Coder <kumarsamarth272@gmail.com>
2024-11-23 15:58:33 +0530

    commit

--- main.py
+++ main.py
@@ -17,3 +17,18 @@ penguin_rect=penguin_image.get_rect(center=(150,370))
 text=pygame.font.Font(None,36).render('Hello World',True,
                                       pygame.Color('black'))
 text_rect=text.get_rect(center=(150,310))
+def game_loop():
+    cloak=pygame.event.get():
+    running=True
+    while running:
+        for event in pygame.event.get():
+            if event.type==pygame.Quit:
+                running=False
+        screen.blit(background_image,(0,0))
+        screen.blit(penguin_image,penguin_rect)
+        screen.blit(text,text_rect)
+
+        pygame.display.flip()
+        clock.tick(30)
+    pygame.quit()
+game_loop
