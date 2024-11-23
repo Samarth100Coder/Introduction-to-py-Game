611c8efa74abcc932b6c0881294a5404c22581a8
Samarth100Coder <kumarsamarth272@gmail.com>
2024-11-23 15:50:48 +0530

    commit

--- main.py
+++ main.py
@@ -7,3 +7,13 @@ while not done:
         if event.type==pygame.QUIT:
             pygame.quit()
     pygame.display.flip()
+pygame.display.set_caption('Adding image and background image')
+background_image=pygame.transform.scale(
+    pygame.image.load('download (9).jpg').convert(),
+    (300,400))
+penguin_image=pygame.transform.scale(
+    pygame.image.load('download.jpg').convert_alpha(), (200,200))
+penguin_rect=penguin_image.get_rect(center=(150,370))
+text=pygame.font.Font(None,36).render('Hello World',True,
+                                      pygame.Color('black'))
+text_rect=text.get_rect(center=(150,310))
